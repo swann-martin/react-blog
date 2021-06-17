24ea8dea1c84baadfff0ca718a103ba707a35df6
swann <swannmartin1988@gmail.com>
2021-06-17 22:28:04 +0200

    added axios and update on state when clicked on button

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,7 +1,7 @@
 // == Import
 import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
-
+import axios from 'axios'
 import PropTypes from 'prop-types';
 
 // Composants
@@ -32,6 +32,43 @@ const Blog = () => {
     return postsList.filter((post) => post.category === label);
   };
 
+  const onPostButtonClick = () => {
+    axios({
+      method: 'get',
+      url: 'https://oclock-open-apis.vercel.app/api/blog/posts'
+    })
+      .then((res) => {
+
+        console.log('res.data', res.data)
+        updateList(res.data, 'posts')
+      })
+      .catch((err) => {
+        console.log(console.log('oups ça ne fonctionne pas', err))
+      })
+  }
+
+  const onCategoriesButtonClick = () => {
+    axios({
+      method: 'get',
+      url: 'https://oclock-open-apis.vercel.app/api/blog/categories'
+    })
+      .then((res) => {
+
+        console.log('res.data', res.data)
+        updateList(res.data, 'categories')
+      })
+      .catch((err) => {
+        console.log(console.log('oups ça ne fonctionne pas', err))
+      })
+  }
+
+  const updateList = (data, stateToUpdate) => {
+    if (stateToUpdate === 'posts') {
+      setPostsList(data);
+    } else if (stateToUpdate === 'categories')
+      setCategoriesList(data)
+  }
+
   return (
     <div className="blog">
       <Header
@@ -39,6 +76,9 @@ const Blog = () => {
         zenMode={zenMode}
         onZenActivationClicked={handleZenMode}
       />
+
+      <button onClick={onPostButtonClick}> Update les posts</button>
+      <button onClick={onCategoriesButtonClick}> Update les posts</button>
       <Switch>
         {categoriesList.map((routeObject) => (
           <Route exact key={routeObject.route} path={routeObject.route}>
